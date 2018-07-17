fe4dec94be2f067c791a46cd4b7d31afdad26c3a
Kyle Smith <kyle.smithy2@gmail.com>
2018-07-17 22:04:20 +1000

    config: Start of ConfigManager logic
    - TODO: JSON parsing error handling.

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -14,5 +14,20 @@ ConfigManager::ConfigManager()
 
 void ConfigManager::loadConfig(std::string cfg_name)
 {
-	
+	std::ifstream cfg_file(cfg_name.c_str());
+
+	if (!cfg_file.is_open())
+	{
+		Notify::instance()->error("Could not find config file: " + cfg_name);
+		return;
+	}
+
+	// Make sure we have an empty data object.
+	this->cfg_data.clear();
+
+	// Read the stream into the data object.
+	cfg_file >> this->cfg_data;
+
+	// Close the config file.
+	cfg_file.close();
 }
--- src/config/ConfigManager.h
+++ src/config/ConfigManager.h
@@ -1,6 +1,8 @@
 #ifndef CONFIG_MANAGER_H
 #define CONFIG_MANAGER_H
 
+#include <nlohmann/json.hpp>
+#include <fstream>
 #include <string>
 
 #include "notifier/Notify.h"
@@ -13,6 +15,7 @@ class ConfigManager
 
 	private:
 		static ConfigManager* _instance;
+		nlohmann::json cfg_data;
 
 		ConfigManager(); // Prevent accidental object creation.
 };
