de5285c4c9672b5bb7f3cb1ef887d7506d33f216
Kyle Smith <kyle.smithy2@gmail.com>
2018-08-25 14:04:29 +1000

    dc: Implemented HashGenerator

--- src/dc/DCHashGenerator.cpp
+++ src/dc/DCHashGenerator.cpp
@@ -1,7 +1,36 @@
 #include "dc/DCHashGenerator.h"
 
 DCHashGenerator::DCHashGenerator()
-{}
+{
+	this->hash = 0;
+	this->index = 0;
+}
 
 DCHashGenerator::~DCHashGenerator()
 {}
+
+void DCHashGenerator::addInt(int size)
+{
+	this->hash = (this->hash + (this->getIndexPrime() * size)) % this->max_hash_size; // Enforce our max hash size.
+	this->index = (this->index + 1) % this->max_index_size; // Enforce our max index limit.
+}
+
+void DCHashGenerator::addString(std::string value)
+{
+	this->addInt(value.length());
+	for (auto i = value.begin(); i != value.end(); ++i)
+	{
+		this->addInt(*i);
+	}
+}
+
+uint32_t DCHashGenerator::getHash()
+{
+	return (this->hash & 0xffffffff);
+}
+
+unsigned int DCHashGenerator::getIndexPrime()
+{
+	// TODO
+	return this->index;
+}
--- src/dc/DCHashGenerator.h
+++ src/dc/DCHashGenerator.h
@@ -10,7 +10,17 @@ class DCHashGenerator
 		DCHashGenerator();
 		~DCHashGenerator();
 
+		void addInt(int size);
+		void addString(std::string value);
+		uint32_t getHash();
+
 	private:
+		const uint32_t max_hash_size = 0xffffffff;
+		const int max_index_size = 10000;
+		uint32_t hash;
+		unsigned int index;
+
+		unsigned int getIndexPrime();
 };
 
 #endif // DC_HASH_GENERATOR_H
